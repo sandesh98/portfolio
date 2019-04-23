17f103f14d673f22cfa961f4bd1376b345c369f1
sandesh <sandeshb981@gmail.com>
2019-04-23 21:46:27 +0200

    Added google analytics

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -23,6 +23,8 @@
 <script src="{{ asset('js/functions.js') }}"></script>
 <script src="{{ asset('modules/titan-slider/titan-slider.js') }}"></script>
 <script src="{{ asset('modules/tera-lightbox/tera-lightbox.js') }}"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-138942259-1"></script>
+<script src="{{ asset('js/custom.js') }}"></script>
 @stack('script')
 </body>
 </html>
