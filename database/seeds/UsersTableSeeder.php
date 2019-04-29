6801f4b05e345fcf03d433ff5f893e7096002b15
sandesh <sandeshb981@gmail.com>
2019-04-29 21:45:21 +0200

    Seeder created

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'sandesh',
+            'email' => 'sandeshb981@gmail.com',
+            'password' => bcrypt('Welcome1234')
+        ]);
+    }
+}
