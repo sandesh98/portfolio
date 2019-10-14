0a7fdae69514d38d48b881770d73c4babc70d059
sandesh <sandeshb981@gmail.com>
2019-10-14 13:36:53 +0200

    Added middleware to dashboard routes

--- app/Http/Controllers/Dashboard/NavbarController.php
+++ app/Http/Controllers/Dashboard/NavbarController.php
@@ -8,6 +8,11 @@ use App\Http\Controllers\Controller;
 
 class NavbarController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function edit()
     {
         $text = Text::first();
--- app/Http/Controllers/Dashboard/ProfileController.php
+++ app/Http/Controllers/Dashboard/ProfileController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function edit()
     {
         return view('dashboard.profile.edit');
