dd26df5d6f4d51875c13b98ce95eafc697d95021
Sandesh <sandeshb981@gmail.com>
2021-03-06 23:15:19 +0100

    removed default browser validation

--- resources/views/website/contact/contact-form.blade.php
+++ resources/views/website/contact/contact-form.blade.php
@@ -2,16 +2,16 @@
     <form id="form" class="vbottom-desktop grid default-form no-spacing lined-form mb-xl">
         @csrf
         <div class="col-3">
-            <input wire:model.lazy="name" required type="text" placeholder="Naam" name="name" id="name" class="form-control">
+            <input wire:model.lazy="name" type="text" placeholder="Naam" name="name" id="name" class="form-control">
             @error('name') <p class="show-error">{{ $message }}</p> @enderror
 
         </div>
         <div class="col-3">
-            <input wire:model.lazy="email" required type="email" placeholder="Email" name="email" id="email" class="form-control">
+            <input wire:model.lazy="email" type="email" placeholder="Email" name="email" id="email" class="form-control">
             @error('email') <p class="show-error">{{ $message }}</p> @enderror
         </div>
         <div class="col-3">
-            <textarea wire:model.lazy="message" required placeholder="Bericht" name="message" id="message" class="small form-control"></textarea>
+            <textarea wire:model.lazy="message" placeholder="Bericht" name="message" id="message" class="small form-control"></textarea>
             @error('message') <p class="show-error">{{ $message }}</p> @enderror
             <textarea wire:model="info" required placeholder="Info" name="info" id="info" class="d-none"></textarea>
         </div>
