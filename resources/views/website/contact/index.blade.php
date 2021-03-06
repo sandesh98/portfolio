a37d30049549507fa1370252b6bdf17f30dcc7d6
Sandesh <sandeshb981@gmail.com>
2021-03-06 23:12:42 +0100

    code cleanup

--- resources/views/website/contact/index.blade.php
+++ resources/views/website/contact/index.blade.php
@@ -15,64 +15,8 @@
                     </div>
 
                     @livewire('contact-form')
-{{--                    <form action="{{ route('contact.store') }}" method="post" id="form" class="vbottom-desktop grid default-form no-spacing lined-form mb-xl">--}}
-{{--                        @csrf--}}
-{{--                        <div class="col-3">--}}
-{{--                            <input required type="text" placeholder="Naam" name="name" id="name" class="form-control">--}}
-{{--                        </div>--}}
-{{--                        <div class="col-3">--}}
-{{--                            <input required type="email" placeholder="Email" name="email" id="email" class="form-control">--}}
-{{--                        </div>--}}
-{{--                        <div class="col-3">--}}
-{{--                            <textarea required placeholder="Bericht" name="message" id="message" class="small form-control"></textarea>--}}
-{{--                        </div>--}}
-{{--                        <div class="col-3">--}}
-{{--                            <input id="send" type="submit" value="Verzenden" class="btn btn-primary">--}}
-{{--                        </div>--}}
-{{--                    </form>--}}
-
-{{--                    <div class="text-center">--}}
-{{--                        <h3 id="response"></h3>--}}
-{{--                    </div>--}}
                 </div>
             </div>
         </section>
     </div>
 @endsection
-
-@push('script')
-    <script>
-        $(document).ready(function () {
-
-            $('#send').click(function (e) {
-                e.preventDefault();
-
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
-
-                $.ajax({
-                    type: "POST",
-                    url: "{{ route('contact.store') }}",
-                    dataType: "json",
-                    data: {
-                        _token: "{{ csrf_token() }}",
-                        name: $('#name').val(),
-                        email: $('#email').val(),
-                        message: $('#message').val(),
-                    },
-                    success: function (data) {
-                        $('#response').html(data);
-
-                        $('#form')[0].reset();
-                    },
-                    error: function () {
-                        $('#response').html('Vul de velden correct in');
-                    }
-                });
-            });
-        });
-    </script>
-@endpush
