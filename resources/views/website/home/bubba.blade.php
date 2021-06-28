65b3451dfdef1626ad6ffb7aadfb49728ed7829b
Sandesh <sandeshb981@gmail.com>
2021-06-28 11:42:16 +0200

    added ga to bubba

--- resources/views/website/home/bubba.blade.php
+++ resources/views/website/home/bubba.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>🐶Bubba🐶</title>
     <style>
         html {
             background: black;
@@ -13,11 +13,12 @@
 
         img {
             max-width: 100%;
-            height: auto; git push --set-upstream origin bubba
+            height: auto;
         }
     </style>
 </head>
 <body>
 <img src="{{ asset('website/images/home/Bubbadedaggoe.jpeg') }}" alt="">
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-138942259-1"></script>
 </body>
 </html>
