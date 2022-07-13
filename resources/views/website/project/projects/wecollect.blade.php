22cb6c818a7c687005742a2378f07fe5e7fa9def
Sandesh <sandeshb981@gmail.com>
2022-07-13 22:56:59 +0200

    testing new project text width

--- resources/views/website/project/projects/wecollect.blade.php
+++ resources/views/website/project/projects/wecollect.blade.php
@@ -77,94 +77,98 @@
         </section>
         <section class="mb-xl">
             <div class="container">
-                <ul class="accordion">
-                    <li class="active">
-                        <div class="head">
-                            <div class="title">Meer informatie</div>
-                        </div>
-                        <div class="body">
-                            <p>Tijdens dit project was ik zelf de baas over het project. Ik mocht namelijk het onderwerp en de uitwerking bepalen. Met dit project wil ik het begrip e-waste naar boven brengen en mensen meer grip geven op wat zij kunnen met hun oude elektronische apparaten kunnen doen. Veel vrienden, familie maar ook klanten die ik gesproken heb, hebben namelijk geen idee wat zij met deze apparaten kunnen doen.</p>
-                            <p>Daarom luidt mijn ontwerpvraag:<br>
-                                <i class="text-muted">”Hoe kunnen consumenten meer inzicht krijgen op hun niet gebruikte elektronische apparaten en daarbij geïnstrueerd worden om deze apparaten te laten recyclen?”</i>
-                            </p>
-                            <p>Eerst ben ik begonnen met deskresearch doen aan de hand van een paar vragen die ik van tevoren had bedacht. Tijdens het onderzoek kwamen erg interessante punten naar boven: Zo is de leeftijd waarop kinderen een telefoon krijgen steeds jonger en jonger en dat ongeveer 10% van alle e-waste tussen het grofvuil beland omdat mensen simpelweg niet weten wat zij met deze apparaten kunnen doen. Deze inzichten heb ik samen met andere inzichten die bijvoorbeeld ook uit mijn Google Form zijn gekomen verwerkt in Statement Cards.</p>
-                            <p>Tijdens het project heb ik samen met een paar klasgenoten een aantal creatieve brainstormsessies (methode van H.G Wells, tijdmachine) gedaan om tot een veelheid aan ideeën te komen. Vervolgens heb ik samen met deze klasgenoot het beste idee gekozen. Om eens goed na te denken over de stappen die ik moet doorlopen om het idee tot een werkelijkheid te maken ben ik in contact geraakt met een Bedrijfskunde student. Samen met haar heb ik de methode: Overbrug het gat, uitgevoerd om na te denken over deze stappen.</p>
-                            <p><b>Het concept:</b><br>
-                                Uit eindelijk ben ik tot het concept Wecollect gekomen. Aan de hand van een netwerk scan kunnen consumenten inzichtelijk krijgen welke apparaten actief en inactief zijn op hun netwerk. Wanneer zij op een apparaat drukken zien zij informatie over het apparaat en wat de mogelijkheden zijn om het apparaat op een verantwoorde manier weg te doen. Na de opties bekeken te hebben als een consument ligt het balletje wel bij hen om vervolgens de stap te nemen om het product daadwerkelijk weg te doen. De stappen die men kan ondernemen zijn als volgt:
-                            </p>
-                            <ul>
-                                <li class="text-muted">Het product inleveren bij een e-waste punt in de buurt.</li>
-                                <li class="text-muted">Een inruil punt vinden om het product te brengen en verkopen.</li>
-                                <li class="text-muted">Het product verkopen via bijvoorbeeld Marktplaats en Facebook.</li>
-                                <li class="text-muted">Instructies om het product terug te zetten naar fabrieksinstellingen.</li>
-                            </ul>
-                            <br>
-
-                            <p><b>Mijn rol</b><br>
-                                Het leukste tijdens het project vond ik het onderzoek doen naar het onderwerp en het prototype van de app. Tijdens het project heb ik mijn skills verder uit weten te breiden door o.a. CBS-statistieken te filteren en wetenschappelijke onderzoeken te lezen. Ook heb ik samen met een ervaren designer gezeten en hebben wij samen de app ontworpen.
-                            </p>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="head">
-                            <div class="title">Tools</div>
-                        </div>
-                        <div class="body">
-                            <div class="grid">
-                                <div class="col-2"><span class="label">Programma's</span>
-                                    <ul class="striped inside">
-                                        <li>Adobe Illustrator</li>
-                                        <li>Adobe Indesign</li>
-                                        <li>Apple Keynote</li>
-                                        <li>Figma</li>
-                                        <li>Miro</li>
-                                        <li>Trello</li>
-                                    </ul>
+                <div class="grid">
+                    <div class="col-6 col-offset-1">
+                        <ul class="accordion">
+                            <li class="active">
+                                <div class="head">
+                                    <div class="title">Meer informatie</div>
                                 </div>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li>
-                        <div class="head">
-                            <div class="title">Methodes</div>
-                        </div>
-                        <div class="body">
-                            <div class="grid">
-                                <div class="col-2"><span class="label">Connect</span>
-                                    <ul class="striped inside">
-                                        <li>Deskresearch</li>
-                                        <li>Destep Context Map</li>
-                                        <li>Statements Cards</li>
-                                        <li>Google Form onderzoek</li>
+                                <div class="body">
+                                    <p>Tijdens dit project was ik zelf de baas over het project. Ik mocht namelijk het onderwerp en de uitwerking bepalen. Met dit project wil ik het begrip e-waste naar boven brengen en mensen meer grip geven op wat zij kunnen met hun oude elektronische apparaten kunnen doen. Veel vrienden, familie maar ook klanten die ik gesproken heb, hebben namelijk geen idee wat zij met deze apparaten kunnen doen.</p>
+                                    <p>Daarom luidt mijn ontwerpvraag:<br>
+                                        <i class="text-muted">”Hoe kunnen consumenten meer inzicht krijgen op hun niet gebruikte elektronische apparaten en daarbij geïnstrueerd worden om deze apparaten te laten recyclen?”</i>
+                                    </p>
+                                    <p>Eerst ben ik begonnen met deskresearch doen aan de hand van een paar vragen die ik van tevoren had bedacht. Tijdens het onderzoek kwamen erg interessante punten naar boven: Zo is de leeftijd waarop kinderen een telefoon krijgen steeds jonger en jonger en dat ongeveer 10% van alle e-waste tussen het grofvuil beland omdat mensen simpelweg niet weten wat zij met deze apparaten kunnen doen. Deze inzichten heb ik samen met andere inzichten die bijvoorbeeld ook uit mijn Google Form zijn gekomen verwerkt in Statement Cards.</p>
+                                    <p>Tijdens het project heb ik samen met een paar klasgenoten een aantal creatieve brainstormsessies (methode van H.G Wells, tijdmachine) gedaan om tot een veelheid aan ideeën te komen. Vervolgens heb ik samen met deze klasgenoot het beste idee gekozen. Om eens goed na te denken over de stappen die ik moet doorlopen om het idee tot een werkelijkheid te maken ben ik in contact geraakt met een Bedrijfskunde student. Samen met haar heb ik de methode: Overbrug het gat, uitgevoerd om na te denken over deze stappen.</p>
+                                    <p><b>Het concept:</b><br>
+                                        Uit eindelijk ben ik tot het concept Wecollect gekomen. Aan de hand van een netwerk scan kunnen consumenten inzichtelijk krijgen welke apparaten actief en inactief zijn op hun netwerk. Wanneer zij op een apparaat drukken zien zij informatie over het apparaat en wat de mogelijkheden zijn om het apparaat op een verantwoorde manier weg te doen. Na de opties bekeken te hebben als een consument ligt het balletje wel bij hen om vervolgens de stap te nemen om het product daadwerkelijk weg te doen. De stappen die men kan ondernemen zijn als volgt:
+                                    </p>
+                                    <ul>
+                                        <li class="text-muted">Het product inleveren bij een e-waste punt in de buurt.</li>
+                                        <li class="text-muted">Een inruil punt vinden om het product te brengen en verkopen.</li>
+                                        <li class="text-muted">Het product verkopen via bijvoorbeeld Marktplaats en Facebook.</li>
+                                        <li class="text-muted">Instructies om het product terug te zetten naar fabrieksinstellingen.</li>
                                     </ul>
-                                </div>
+                                    <br>
 
-                                <div class="col-2"><span class="label">Concept</span>
-                                    <ul class="striped inside">
-                                        <li>Creatieve sessie H.G. Wells</li>
-                                        <li>Pecha Kucka presentatie</li>
-                                        <li>Reframing</li>
-                                        <li>Ontwerprichtlijnen</li>
-                                        <li>Stakeholder Map</li>
-                                        <li>Persona</li>
-                                    </ul>
+                                    <p><b>Mijn rol</b><br>
+                                        Het leukste tijdens het project vond ik het onderzoek doen naar het onderwerp en het prototype van de app. Tijdens het project heb ik mijn skills verder uit weten te breiden door o.a. CBS-statistieken te filteren en wetenschappelijke onderzoeken te lezen. Ook heb ik samen met een ervaren designer gezeten en hebben wij samen de app ontworpen.
+                                    </p>
                                 </div>
+                            </li>
+                            <li>
+                                <div class="head">
+                                    <div class="title">Tools</div>
+                                </div>
+                                <div class="body">
+                                    <div class="grid">
+                                        <div class="col-2"><span class="label">Programma's</span>
+                                            <ul class="striped inside">
+                                                <li>Adobe Illustrator</li>
+                                                <li>Adobe Indesign</li>
+                                                <li>Apple Keynote</li>
+                                                <li>Figma</li>
+                                                <li>Miro</li>
+                                                <li>Trello</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
 
-                                <div class="col-2"><span class="label">Create</span>
-                                    <ul class="striped inside">
-                                        <li>Overbrug het gat methode</li>
-                                        <li>Sitemap</li>
-                                        <li>Moodboard</li>
-                                        <li>Wireframe</li>
-                                        <li>Testplan</li>
-                                        <li>Prototype</li>
-                                    </ul>
+                            <li>
+                                <div class="head">
+                                    <div class="title">Methodes</div>
                                 </div>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
+                                <div class="body">
+                                    <div class="grid">
+                                        <div class="col-2"><span class="label">Connect</span>
+                                            <ul class="striped inside">
+                                                <li>Deskresearch</li>
+                                                <li>Destep Context Map</li>
+                                                <li>Statements Cards</li>
+                                                <li>Google Form onderzoek</li>
+                                            </ul>
+                                        </div>
+
+                                        <div class="col-2"><span class="label">Concept</span>
+                                            <ul class="striped inside">
+                                                <li>Creatieve sessie H.G. Wells</li>
+                                                <li>Pecha Kucka presentatie</li>
+                                                <li>Reframing</li>
+                                                <li>Ontwerprichtlijnen</li>
+                                                <li>Stakeholder Map</li>
+                                                <li>Persona</li>
+                                            </ul>
+                                        </div>
+
+                                        <div class="col-2"><span class="label">Create</span>
+                                            <ul class="striped inside">
+                                                <li>Overbrug het gat methode</li>
+                                                <li>Sitemap</li>
+                                                <li>Moodboard</li>
+                                                <li>Wireframe</li>
+                                                <li>Testplan</li>
+                                                <li>Prototype</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </section>
         <section class="prev-next-container mb-xl">
